277a48568c951e1895795f24fdaeea801c0aaf72
e502692 <daniel.schreiber@sbb.ch>
2025-02-25 19:45:08 +0100

    Update .gitignore and .npmignore; add index.ts and update package.json for improved module exports and versioning

---
+++ index.ts
@@ -0,0 +1,12 @@
+
+
+export * from './model/xmlAttribute';
+export * from './model/xmlCData';
+export * from './model/xmlComment';
+export * from './model/xmlDoctype';
+export * from './model/xmlDocument';
+export * from './model/xmlElement';
+export * from './model/xmlNode';
+export * from './model/xmlProcessing';
+export * from './model/xmlText';
+export * from './xmlParser';
